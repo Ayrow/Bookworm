86fadcb3cab18ec1a69587c94981f1b7cbdefb86
Aymeric <aymeric.pilaert@hotmail.fr>
2023-03-08 13:56:17 +0100

    Setting up a basic add book view

---
+++ Bookworm/AddBookView.swift
@@ -0,0 +1,60 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Aymeric Pilaert on 08/03/2023.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    @Environment(\.managedObjectContext) var moc
+    @State private var title = ""
+    @State private var author = ""
+    @State private var rating = 3
+    @State private var genre = ""
+    @State private var review = ""
+    
+    let genres = ["Fantasy", "Horror", "Kids", "Romance", "Thriller", "Mystery", "Poetry"]
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Name of book", text: $title)
+                    TextField("Author's name", text: $author)
+                    Picker("Genre", selection: $genre) {
+                        ForEach(genres, id:\.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                
+                Section {
+                    TextEditor(text: $review)
+                    Picker("Rating", selection: $rating) {
+                        ForEach(0..<6) {
+                            Text(String($0))
+                        }
+                    }
+                    
+                } header: {
+                    Text("Write a review")
+                }
+                
+                Section {
+                    Button("Save") {
+                        //
+                    }
+                }
+            }
+            .navigationTitle("Add Book")
+        }
+    }
+}
+
+struct AddBookView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddBookView()
+    }
+}
