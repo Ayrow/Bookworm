d1f483889e1c0a86ab3a9ca5a7a9180859289faf
Aymeric <aymeric.pilaert@hotmail.fr>
2023-03-09 14:59:34 +0100

    Added delete function to delete book from core data as well as edit button

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -40,10 +40,14 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks)
             }
-            
                 .navigationTitle("Bookworm")
                 .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        EditButton()
+                    }
+                    
                     ToolbarItem(placement: .navigationBarTrailing) {
                         Button {
                             showingAddScreen.toggle()
@@ -57,6 +61,16 @@ struct ContentView: View {
                 }
         }
     }
+    
+    func deleteBooks(at offsets: IndexSet){
+        for offset in offsets {
+            let book = books[offset]
+            moc.delete(book)
+            
+        }
+        try? moc.save()
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
